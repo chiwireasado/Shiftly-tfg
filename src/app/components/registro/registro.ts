195b3b526769c82218f2591ab9b75bd32baeff98
chiwireasado <mariajoseramirezperez1@gmail.com>
2026-02-19 10:34:06 +0100

    panel-control modificado con acceso user y admin, registro con edicion

--- src/app/components/registro/registro.ts
+++ src/app/components/registro/registro.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class Registro {
 
+  cambiarforma(variable: string) {
+    let primerForm = document.querySelector('.register-form');
+    let secondForm = document.querySelector('.register-form-second');
+
+    if (variable === 'segundo' ) {
+      primerForm?.classList.remove('active');
+      secondForm?.classList.add('active');
+    } else {
+      secondForm?.classList.remove('active');
+      primerForm?.classList.add('active');
+    }
+  }
+
 }
